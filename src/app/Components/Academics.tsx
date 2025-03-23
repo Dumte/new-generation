f1b6c466020566000c14c4f0f5835ad676ce302b
dumte <dummy4real@gmail.com>
2025-03-24 00:14:09 +0100

    Update Academics section layout for improved alignment and centering

--- src/app/Components/Academics.tsx
+++ src/app/Components/Academics.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { academicsLevels } from "../data";
 const Academics = () => {
   return (
-    <section id="academics" className="py-8 w-full max-w-[80%] mx-auto bg-blue-100">
+    <section id="academics" className="py-8 w-full max-w-[80%] flex items-center justify-center bg-blue-100">
       <div className="grid grid-cols-1 gap-6 md:grid-cols-3 mx-2 ">
         {academicsLevels.map((level) => (
          <Link href={level.href} key={level.name} className="flex justify-center items-center w-[300px] h-[200px] rounded-lg bg-blue-500 p-6 text-center shadow-md transition-all hover:bg-blue-200 hover:shadow-lg">
