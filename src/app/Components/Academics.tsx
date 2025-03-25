0e028e986563b5c90c27ae7c146a712259778e88
Dumte Nwidoobee Joshua <61066433+Dumte@users.noreply.github.com>
2025-03-25 20:00:26 +0100

    Update Academics.tsx

--- src/app/Components/Academics.tsx
+++ src/app/Components/Academics.tsx
@@ -5,7 +5,7 @@ const Academics = () => {
     <section id="academics" className="py-8 w-full max-w-[80%] bg-blue-100 mx-auto">
       <div className="grid grid-cols-1 gap-6 md:grid-cols-3 mx-2 ">
         {academicsLevels.map((level) => (
-         <Link href={level.href} key={level.name} className=" w-[300px] h-[200px] rounded-lg bg-blue-500 p-6 text-center shadow-md transition-all hover:bg-blue-200 hover:shadow-lg">
+         <Link href={level.href} key={level.name} className=" w-full h-64 rounded-lg bg-blue-500 p-6 text-center shadow-md transition-all hover:bg-blue-200 hover:shadow-lg">
             <h3 className="text-xl font-semibold">{level.name}</h3>
         </Link> 
         ))
