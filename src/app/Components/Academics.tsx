1039ae2692dfd3de68e447a8a2a3238d556f4f74
Dumte Nwidoobee Joshua <61066433+Dumte@users.noreply.github.com>
2025-04-01 07:00:45 +0100

    Update Academics.tsx

--- src/app/Components/Academics.tsx
+++ src/app/Components/Academics.tsx
@@ -1,32 +1,34 @@
+import Image from "next/image";
 import Link from "next/link";
 import { academicsLevels } from "../data";
+
 const Academics = () => {
   return (
     <section id="academics" className="mx-auto py-8 w-full max-w-[90%]">
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-3 mx-2 lg:grid-cols-3 ">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-3 mx-2 lg:grid-cols-3">
         {academicsLevels.map((level) => (
           <Link
             key={level.name}
             href={level.href}
             className="group relative h-64 overflow-hidden rounded-lg shadow-md transition-all hover:shadow-lg"
           >
-            {/* Background Image */}
-            <div
-              className="h-full w-full bg-cover transition-all group-hover:scale-105"
-              style={{ backgroundImage: `url(${level.image})` }}
-            ></div>
+            {/* Optimized Image */}
+            <div className="h-full w-full relative">
+              <Image
+                src={level.image}
+                alt={`Image for ${level.name} level`}
+                layout="fill"
+                objectFit="cover"
+                className="transition-transform group-hover:scale-105"
+              />
+            </div>
 
             {/* Overlay */}
             <div className="absolute inset-0 flex flex-col items-center justify-end bg-black bg-opacity-40 p-4 text-center transition-all group-hover:opacity-60">
-              {/* Title */}
               <h3 className="text-xl font-semibold text-white">{level.name}</h3>
-
-              {/* Description */}
-              <div className="mt-2 overflow-hidden">
-                <p className="translate-y-8 transform text-sm text-white opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100">
-                  {level.description}
-                </p>
-              </div>
+              <p className="translate-y-8 transform text-sm text-white opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100">
+                {level.description}
+              </p>
             </div>
           </Link>
         ))}
